8f82fbfe50e2dc2baa646188023463e2a2924f14
Deeksha Verma <deekshaverma.dv79@gmail.com>
2026-04-16 18:45:35 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // frontend URL
+ origin: [
+    'http://localhost:5173',
+    'https://task-manager-frontend-6piw.vercel.app'
+  ],
     credentials: true,
   });
    app.setGlobalPrefix('api'); // ✅ ADD THIS
